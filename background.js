446e77c3fb0bebbd49b2c15f76828966f64d5dfa
2boom <97739181+2boom-ua@users.noreply.github.com>
2026-08-23 17:21:42 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 // CSS Color Converter
 // Copyright 2boom, 2026
 
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error("Error init sidebar:", error));
+chrome.runtime.onInstalled.addListener(() => {
+  if (chrome.sidePanel && chrome.sidePanel.setPanelBehavior) {
+    chrome.sidePanel
+      .setPanelBehavior({ openPanelOnActionClick: true })
+      .catch((error) => console.error("Error init sidebar:", error));
+  }
+});
